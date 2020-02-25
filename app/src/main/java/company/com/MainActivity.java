691314eb527b27e49f4d6d4728f73587e948d26d
Mcpwko <mcpwko@hotmail.fr>
2020-02-25 13:54:23 +0100

    Creation of the project 0.0.1

---
+++ app/src/main/java/company/com/MainActivity.java
@@ -0,0 +1,22 @@
+package company.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "Welcome User";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void connect(View view) {
+        Intent intent = new Intent(this, HomeConected.class);
+        startActivity(intent);
+    }
+}
